1690eed0f87b8b1c6bd2f19ba7ea7c8e8852beaa
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-06-15 14:06:43 +0100

    Add check & binding options to aatestprog.F90

--- lam.sh
+++ lam.sh
@@ -10,10 +10,21 @@ module unload gnu
 module load nvhpc/20.9
 
 
+if [ 0 -eq 1 ]
+then
+
+export DR_HOOK_NOT_MPI=1
+gdb --args ./bin/AATESTPROG --no-write \
+  --namelist fort.4.20x20 --lmpoff \
+  --time 10 # > AATESTPROG.eo 2>&1
+
+
+fi
+
 #--alloperm \
 mpirun -np 2 ./bin/AATESTPROG --no-write \
-  --namelist fort.4.1000x1000 \
-  --time 10 # > AATESTPROG.eo 2>&1
+  --namelist fort.4.100x100 \
+  --time 10 --check # > AATESTPROG.eo 2>&1
 
 exit
 
