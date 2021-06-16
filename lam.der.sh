e9c5314c73bbb7fd7836747d0b624ed9e118fac9
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-06-16 12:30:42 +0100

    Derivatives work with CUDA-aware MPI

---
+++ lam.der.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ulimit -s unlimited
+export OMP_NUM_THREADS=1
+
+set -x
+set -e
+
+module unload gnu
+module load nvhpc/20.9
+
+
+mpirun -np 1 ./bin/AATESTPROGDER \
+  --namelist fort.4.20x20 \
+  --time 1 
+
+IP=86.201.29.85
+
+for f in *.fa
+do
+  cp $f ~/tmp/
+  ssh ecgate scp ~/tmp/$f phi001@$IP:tmp/$f
+done
+
